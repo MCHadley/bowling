b346571234c50c3e926cd45095a14887180dac03
Michael <michael.c.hadley@gmail.com>
2018-04-09 11:46:25 -0400

    "created registration form"

---
+++ registeration-form.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+        <form action="register.php" method="post">
+            <h1>Registration</h1>
+            First Name:
+            <input type="text" id="firstName" name="firstName"><br>
+            Last Name:
+            <input type="text" id="lastName" name="lastName"><br>
+            Email: 
+            <input type="text" id="email" name="email"><br>
+            Password: 
+            <input type="password" id="password" name="password"><br>
+            <input type="submit" value="submit">
+        
+        </form>
+    </body>
+</html>
