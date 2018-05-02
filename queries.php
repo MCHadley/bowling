7d2971ec63f2234d4455f41e1b1424d82e2fec00
Michael <michael.c.hadley@gmail.com>
2018-05-02 10:36:49 -0400

    changed file name

--- queries.php
+++ queries.php
@@ -38,7 +38,7 @@
                     $_SESSION['pass'] = $row['pass'];
                     
                     echo '<p><a href="logout.php">Logout</a>' . " " . $_SESSION['email'] . '</p>';
-                    echo '<p><a href="show-bowlers.form.php">Show Bowlers</a></p>';
+                    echo '<p><a href="show-bowlers-form.php">Show Bowlers</a></p>';
 
                 }
             else
